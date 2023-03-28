8a28bb0818d8b3ffac9f01afa386912ecf13bca2
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-03-28 08:12:21 +0200

    Declarada la funcion Game que controla el flujo de juego

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const gameBoard = (() => {
-    
-    const length = 9;
-    const board = [];
+  const length = 9;
+  const board = [];
 
-    for(let i = 0; i < length; i++){
-       board[i] = '';
-    }
+  for (let i = 0; i < length; i++) {
+    board[i] = '';
+  }
 
-    const getBoard = () => board;
-    const setMark = (player,index) => board[index] = player.getMark();
-    
-    return {getBoard, setMark};
+  const getBoard = () => board;
+  const setMark = (player, index) => (board[index] = player.getMark());
+  const setBoard = (player1, player2) =>  {
+   for(let i = 0; i < board.length; i++){
+    Math.random() < 0.49 ? board[i] = player1.getMark() : board[i] = player2.getMark();
+   }
+  }
+
+  return { getBoard, setMark, setBoard };
 })();
 
-const Player = (playerMark='') => {
-    const mark = playerMark;
+const Player = (playerMark = "") => {
+  const mark = playerMark;
 
-    const getMark = () => mark;
-    return {getMark};
-
+  const getMark = () => mark;
+  return { getMark };
 };
 
+function Game(playerOne = 'X', playerTwo = 'O') {
+    const players = [Player(playerOne), Player(playerTwo)];
+    gameBoard.setBoard(players[0], players[1]);
+    console.log(gameBoard.getBoard());
+}
 
-
-
-
+Game();
 
