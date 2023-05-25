a4ccbdcb88120c1f7c355ad641e62e5c8cb90511
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-05-25 15:20:50 +0200

    Condiciones de ganar, reset y estabelecer el tablero, switch player y playRound

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const winningConditions = [
+  [0,1,2],
+  [0,4,8],
+  []
+]
+
 const gameBoard = (() => {
   const board = [];
   const length = 9;
-  for (let i = 0; i < length; i++){
-    board[i] = '';
+  const setBoard = () =>{
+    for (let i = 0; i < length; i++){
+      board[i] = '';
+    }
   }
+  setBoard();
+
+  const resetBoard = () => setBoard();
+  
 
   const getBoard = () => board;
   const setMark = (mark, index) => board[index] = mark;
   
-  return {getBoard, setMark}
+  return {getBoard, setMark, resetBoard}
 })();
 
 function Player(mark, name){
@@ -25,19 +37,49 @@ const gameController = (() =>{
     Player('O', 'Player Two')
   ]
 
-  let activePlayer = player[0];
+  let activePlayer = 0;
 
   const switchPlayer = () => {
-    activePlayer === players[0] ? players[1] : players[0];
+    activePlayer = (activePlayer + 1) % 2;
   }
   const playRound = (cell) => {
-    if(board[cell] === ''){
-
-    }else{
-      console.log('The cell is already marked');
+    if(cell > board.length - 1 || cell < 0){
+      console.log('The cell is off limits');
+      return;
     }
+
+    if(board[cell] !== ''){
+      console.log('The cell is already marked');
+      return;
+    }
+
+    board[cell] = players[activePlayer].mark;
+    console.log(checkWinner());
+    switchPlayer();
+    console.log(printBoard());
+    return;
   }
-  return {players}
+
+  const whichTurn = () => {
+    return `It's ${players[activePlayer].name}'s turn.`
+  }
+  const printBoard = () => {
+    console.log(whichTurn());
+    let printedBoard = '';
+    
+    for(let i = 0; i < board.length; i++){
+      if (i % 3 === 0) {
+        printedBoard += '\n';
+      }
+      printedBoard += ` [${board[i]}] `;
+    }
+    return printedBoard;
+  }
+  return {playRound, whichTurn}
 })()
 
-console.log(gameController.players);
+function checkWinner(){
+  
+  return true;
+}
+console.log(gameController.whichTurn());
