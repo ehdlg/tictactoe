e689878f986223a1c512c55c8c7f5bec8387b843
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-05-25 15:47:12 +0200

    fucntion getMarkedCells para obtener las celdas marcadas del jugador actual

--- index.js
+++ index.js
@@ -1,85 +1,99 @@
 const winningConditions = [
-  [0,1,2],
-  [0,4,8],
-  []
-]
-
+  [0, 1, 2], // Fila 1
+  [3, 4, 5], // Fila 2
+  [6, 7, 8], // Fila 3
+  [0, 3, 6], // Columna 1
+  [1, 4, 7], // Columna 2
+  [2, 5, 8], // Columna 3
+  [0, 4, 8], // Diagonal principal
+  [2, 4, 6], // Diagonal secundaria
+];
+/**
+ * IIFE para el tablero
+ */
 const gameBoard = (() => {
+  //Array tablero
   const board = [];
+  //Longitud del tablero
   const length = 9;
-  const setBoard = () =>{
-    for (let i = 0; i < length; i++){
-      board[i] = '';
+  //Funcion que establece las celdas del tablero como string vacíos
+  const setBoard = () => {
+    for (let i = 0; i < length; i++) {
+      board[i] = "";
     }
-  }
+  };
+  //Se llama a la función setBoard para establecer el tablero
   setBoard();
-
+  //Funcion que reinicar el tablero llamando a setBoard()
   const resetBoard = () => setBoard();
-  
-
+  //Funcion que devuevle el tablero
   const getBoard = () => board;
-  const setMark = (mark, index) => board[index] = mark;
-  
-  return {getBoard, setMark, resetBoard}
+  //Funcion que establece la marca del jugador en el indice indicado
+  const setMark = (mark, index) => (board[index] = mark);
+  //Devuelve las funciones getBoard, setMark y resetBoard
+  return { getBoard, setMark, resetBoard };
 })();
-
-function Player(mark, name){
+//Factory function para los jugadores, devuevel un objeto con los parametros mark y name
+function Player(mark, name) {
   return {
     mark,
-    name
-  }
+    name,
+  };
 }
-
-const gameController = (() =>{
+//IIFE para el controlador del juego
+const gameController = (() => {
+  //Variable que contiene el tablero de gameBoard
   const board = gameBoard.getBoard();
-  const players = [
-    Player('X', 'Player One'),
-    Player('O', 'Player Two')
-  ]
-
+  //Array con los dos objetos jugadores
+  const players = [Player("X", "Player One"), Player("O", "Player Two")];
+  //Turno del jugador, por defecto jugador uno (indice 0)
   let activePlayer = 0;
-
+  //Funcion que dependiendo del activePlayer, cambia de turno al sumar por 1 y hacer el modulo
   const switchPlayer = () => {
     activePlayer = (activePlayer + 1) % 2;
-  }
+  };
+  //Funcion que establece una ronda de juego
   const playRound = (cell) => {
-    if(cell > board.length - 1 || cell < 0){
-      console.log('The cell is off limits');
+    //Si la celda indicada está fuera del limite, se para la funcion
+    if (cell > board.length - 1 || cell < 0) {
+      console.log("The cell is off limits");
       return;
     }
-
-    if(board[cell] !== ''){
-      console.log('The cell is already marked');
+    //aslfjslaej
+    //Si la celda no está vacía (ya está marcada), se para la funcion
+    if (board[cell] !== "") {
+      console.log("The cell is already marked");
       return;
     }
-
+    //En caso de que no haya ningun error en la celda, se procede a establecer la marca del jugador en el tablero
     board[cell] = players[activePlayer].mark;
-    console.log(checkWinner());
+    if(checkWinner()){
+
+    }
     switchPlayer();
     console.log(printBoard());
     return;
-  }
+  };
 
   const whichTurn = () => {
-    return `It's ${players[activePlayer].name}'s turn.`
-  }
+    return `It's ${players[activePlayer].name}'s turn.`;
+  };
   const printBoard = () => {
     console.log(whichTurn());
-    let printedBoard = '';
-    
-    for(let i = 0; i < board.length; i++){
+    let printedBoard = "";
+
+    for (let i = 0; i < board.length; i++) {
       if (i % 3 === 0) {
-        printedBoard += '\n';
+        printedBoard += "\n";
       }
       printedBoard += ` [${board[i]}] `;
     }
     return printedBoard;
-  }
-  return {playRound, whichTurn}
-})()
+  };
+  return { playRound, whichTurn };
+})();
 
-function checkWinner(){
-  
+function checkWinner() {
   return true;
 }
 console.log(gameController.whichTurn());
